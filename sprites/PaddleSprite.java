e4331b5a61828f9efa0952ece945588027e519e0
1100053835 <1100053835@L829WD210006.edu.cbe.ab.ca>
2022-12-14 12:44:22 -0700

    test commit

---
+++ sprites/PaddleSprite.java
@@ -0,0 +1,83 @@
+import java.awt.Image;
+
+public class PaddleSprite implements DisplayableSprite {
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getMinX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMinY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCenterX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getCenterY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getDispose() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setDispose(boolean dispose) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Universe universe, KeyboardInput keyboard, long actual_delta_time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
